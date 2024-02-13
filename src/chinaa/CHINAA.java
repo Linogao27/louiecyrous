3eb3c907e4760172b850379a07fd796e426934f1
Student <Student@CLB3Unit23>
2024-02-13 14:45:26 +0800

    Version 1.0

---
+++ src/chinaa/CHINAA.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chinaa;
+
+/**
+ *
+ * @author Student
+ */
+public class CHINAA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String firstName = "Adrianna";
+        String middleName = "Perti Ka";
+        String lastName = "OAAAAAA";
+        char gender = 'F';
+        int age = 17; 
+        boolean isHuman = false;
+        
+           System.out.println(firstName + " " + middleName + " " + lastName);
+        
+           System.out.println("First Name: " + firstName);
+           System.out.println("Middle Name: " + middleName);
+           System.out.println("Last Name: " + lastName);
+           System.out.println("Gender: " + gender);
+           System.out.println("Age: " + age);
+           System.out.println("Tao Ba Ka: + isHuman");
+                
+      
+         
+        
+
+                 
+                 
+    }
+    
+}
+              
